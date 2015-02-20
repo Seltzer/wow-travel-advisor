5155ad44b5eee61ce1887c8d6824f94dc8df656f
Seltzer <pitmannathan@gmail.com>
2015-02-20 20:20:08 +1300

    Rename files to meet WOW's lofty standards

---
+++ TravelAdvisor.lua
@@ -0,0 +1,9 @@
+﻿function TravelAdvisor_OnLoad()
+   print("Hello, World!");
+end
+
+
+SLASH_TRAVELADVISOR1, SLASH_TRAVELADVISOR2 = '/ta', '/travel-advisor';
+function SlashCmdList.TRAVELADVISOR(msg, editbox)
+   print("Welcome to TA!");
+end
