288e1a3f8a0f4e63d0d84053d94842524e1b63a1
Seltzer <pitmannathan@gmail.com>
2015-02-20 22:15:57 +1300

    Start on TA.GetTravelItemsOnPlayer(). Plus general L2LUA

--- TravelAdvisor.lua
+++ TravelAdvisor.lua
@@ -1,50 +1,63 @@
-﻿TA = LibStub('AceAddon-3.0'):NewAddon('Travel Advisor')
+﻿-- Initialise TA addon/module
+TA = LibStub('AceAddon-3.0'):NewAddon('Travel Advisor')
+
+-- Set up TA submodule stubs
+TA.LuaUtils = {}
+
+-- Forward declare private functions
+local setUpSlashCommand, hasItem
 
 
 
-local function hasItem(itemId)
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Public functions, exposed via TA module
+-----------------------------------------------------------------------------------------------------------------------
+
+function TA.OnLoad() 
+   print('TA.OnLoad')
+   
+   setUpSlashCommand()
+end
+
+
+-------------------------------------------------------------------------------
+-- Gets a table representing all of the travel-relevant items which the player 
+-- has on them.
+-------------------------------------------------------------------------------
+function TA.GetTravelItemsOnPlayer()
+   local inv = { }
+
+   inv.gadgetzan = hasItem(18986)
+
+   return inv
+end
+
+
+function TA.OnSlashInvocation()
+   print('Welcome to TA!')
+
+   local inv = TA.GetTravelItemsOnPlayer()
+
+   TA.LuaUtils.PrintTable(inv)
+end
+
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------
+-- Private functions
+-----------------------------------------------------------------------------------------------------------------------
+setUpSlashCommand = function ()
+   SLASH_TRAVELADVISOR1, SLASH_TRAVELADVISOR2 = '/ta', '/travel-advisor'
+
+   function SlashCmdList.TRAVELADVISOR(msg, editbox)
+      TA.OnSlashInvocation()
+   end
+end
+
+
+hasItem = function(itemId)
    return GetItemCount(itemId, false, false) > 0
 end
-
-
-function TravelAdvisor_OnLoad()
-   print("Hello, World!");
-end
-
-
-function TA.TestFunc()
-   print('TravelAdvisoraaa.TestFunc')
-end
-
-
-
------------------------------------------------------------------------------
---
------------------------------------------------------------------------------
--- function TravelAdvisor.GetTravelInventory()
---    print('entry');
---    local inv = { }
-
-   
---    print('hello')
-
---    -- 18986
---    inv.gadgetzan = hasItem(18986)
-
---    LuaUtils.PrintTable(inv)
--- end
-
-
-
-
-
-SLASH_TRAVELADVISOR1, SLASH_TRAVELADVISOR2 = '/ta', '/travel-advisor';
-function SlashCmdList.TRAVELADVISOR(msg, editbox)
-   print("Welcome to TA!");
-
-   test()
-   TA.TestFunc()
-   --TravelAdvisor.GetTravelInventory();
-end
-
-
