3fa1f904d536c1470fe301821fb8d1a233e53c10
Seltzer <pitmannathan@gmail.com>
2015-02-20 22:27:48 +1300

    Added deploy.ps1 script

---
+++ deploy.ps1
@@ -0,0 +1,9 @@
+$wowAddonBase = "C:\Program Files (x86)\World of Warcraft\Interface\AddOns\TravelAdvisor\"
+
+rm -R "$wowAddonBase*"
+
+copy .\TravelAdvisor.toc $wowAddOnBase
+copy .\TravelAdvisor.xml $wowAddOnBase
+copy .\TravelAdvisor.lua $wowAddOnBase
+copy .\LuaUtils.lua $wowAddOnBase
+copy .\lib $wowAddOnBase
